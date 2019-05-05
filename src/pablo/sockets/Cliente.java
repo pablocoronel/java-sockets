12357464fb27a6f2f43252721b7972ada47a2071
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-05-04 22:27:55 -0300

    initial

---
+++ src/pablo/sockets/Cliente.java
@@ -0,0 +1,55 @@
+package pablo.sockets;
+
+import javax.swing.*;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MarcoCliente mimarco = new MarcoCliente();
+
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
+
+class MarcoCliente extends JFrame {
+
+	public MarcoCliente() {
+
+		setBounds(600, 300, 280, 350);
+
+		LaminaMarcoCliente milamina = new LaminaMarcoCliente();
+
+		add(milamina);
+
+		setVisible(true);
+	}
+
+}
+
+class LaminaMarcoCliente extends JPanel {
+
+	public LaminaMarcoCliente() {
+
+		JLabel texto = new JLabel("CLIENTE");
+
+		add(texto);
+
+		campo1 = new JTextField(20);
+
+		add(campo1);
+
+		miboton = new JButton("Enviar");
+
+		add(miboton);
+
+	}
+
+	private JTextField campo1;
+
+	private JButton miboton;
+
+}
